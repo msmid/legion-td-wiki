510ccf2b927f8a2b0cad6b0b16d4fa6f01804ff0
Martin Smid <smm@panter.ch>
2024-09-14 14:50:50 +0200

    chore: remove ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,13 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_GB } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { IconDefinition } from '@ant-design/icons-angular';
-import * as AllIcons from '@ant-design/icons-angular/icons';
-
 import { UnitsModule } from './units/units.module';
-
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition;
-};
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 registerLocaleData(en);
 
@@ -33,11 +19,8 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzLayoutModule,
-    NzIconModule.forRoot(icons),
     UnitsModule,
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_GB }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/units/units.module.ts
+++ src/app/units/units.module.ts
@@ -1,37 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DataServiceService } from './data-service.service';
-import { UnitItemComponent } from './components/unit-item/unit-item.component';
-
-import { IconDefinition } from '@ant-design/icons-angular';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-
-// TODO refactor to use its own module because it is duplicate it in app.module
-import * as AllIcons from '@ant-design/icons-angular/icons';
+import { NgModule } from '@angular/core';
 import { UnitDetailModalComponent } from './components/unit-detail-modal/unit-detail-modal.component';
-
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition;
-};
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
+import { UnitItemComponent } from './components/unit-item/unit-item.component';
+import { DataServiceService } from './data-service.service';
 
 @NgModule({
   declarations: [UnitItemComponent, UnitDetailModalComponent],
   imports: [
     CommonModule,
-    NzCardModule,
-    NzTagModule,
-    NzDescriptionsModule,
-    NzIconModule.forRoot(icons),
-    NzToolTipModule,
-    NzModalModule,
-    NzGridModule,
   ],
   providers: [DataServiceService],
   exports: [UnitItemComponent],
